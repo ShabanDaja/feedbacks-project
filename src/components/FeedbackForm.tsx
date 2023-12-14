48b1d9bf7160e590e8708a01817befabb25b6216
Shaban Daja <shabandaja710@gmail.com>
2023-12-14 10:08:14 +0100

    Implement character count with UseState and controlled input.

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+  const charCount = MAX_CHARACTERS - text.length;
   return (
     <form className='form'>
-      <textarea id='feedback-textarea' placeholder='ex' spellCheck={false} />
+      <textarea
+        value={text}
+        onChange={e => {
+          setText(e.target.value);
+        }}
+        id='feedback-textarea'
+        placeholder='ex'
+        spellCheck={false}
+      />
       <label htmlFor='feedback-textarea'>
         Enter your feedback here, remember to #hashtag the company.
       </label>
       <div>
-        <p className='u-italic'>150</p>
+        <p className='u-italic'>{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
