c189c03eb732cc4883b035fa02a71530e58fc1db
Shaban Daja <shabandaja710@gmail.com>
2023-12-14 11:41:37 +0100

    Implement error message when fetching data.

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
@@ -12,10 +14,17 @@ export default function FeedbackList() {
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
     )
       .then(res => {
+        if (!res.ok) {
+          throw new Error();
+        }
         return res.json();
       })
       .then(data => {
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setErrorMessage("Something went wrong.");
         setIsLoading(false);
       });
   }, []);
@@ -23,6 +32,9 @@ export default function FeedbackList() {
   return (
     <ol className='feedback-list'>
       {isLoading ? <Spinner /> : null}
+
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
+
       {feedbackItems.map(feedbackItem => (
         <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
