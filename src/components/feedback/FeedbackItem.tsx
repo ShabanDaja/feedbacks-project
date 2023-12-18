274a757ed5132b04b72144707338191e1d5f47d1
Shaban Daja <shabandaja710@gmail.com>
2023-12-18 10:58:01 +0100

    Upvote feedback item functionality.

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -8,14 +8,21 @@ type FeedbackItemProps = {
 
 export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
   const [open, setOpen] = useState(false);
+  const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount);
+
+  const handleUpvote = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setUpvoteCount(prev => ++prev);
+    e.currentTarget.disabled = true;
+    e.stopPropagation();
+  };
 
   return (
     <li
       onClick={() => setOpen(prev => !prev)}
       className={`feedback ${open ? "feedback--expand" : ""}`}>
-      <button>
+      <button onClick={handleUpvote}>
         <TriangleUpIcon />
-        <span>{feedbackItem.upvoteCount}</span>
+        <span>{upvoteCount}</span>
       </button>
       <div>
         <p>{feedbackItem.badgeLetter}</p>
